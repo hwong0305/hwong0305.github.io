4d4422ac1ff474e26f853ba2ae02a9647bdaf8ce
hwong0305 <hwong.0305@gmail.com>
2019-10-17 07:03:41 -0700

    Changed Kanban App to Drag and Drop React version

---
+++ kanban/precache-manifest.18ddefb20ff047c5ef3b9e2a0bb1f6fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d044329f1e95ee1a823901347355dd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb86ccc63c13f48daa19",
+    "url": "/static/css/main.7a914354.chunk.css"
+  },
+  {
+    "revision": "f09f2f50487903eefd4c",
+    "url": "/static/js/2.da2167d0.chunk.js"
+  },
+  {
+    "revision": "cb86ccc63c13f48daa19",
+    "url": "/static/js/main.d99dedac.chunk.js"
+  },
+  {
+    "revision": "ad225031105cfe33ab5c",
+    "url": "/static/js/runtime-main.ff777d46.js"
+  }
+]);
